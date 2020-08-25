62219fad6454fea38aa4eaaf4402b013bf4db3c2
githubfoam <lequestionis@gmail.com>
2020-08-25 19:52:04 +0300

    typos

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -o errexit
-set -o pipefail
-set -o nounset
-set -o xtrace
+# set -o errexit
+# set -o pipefail
+# set -o nounset
+# set -o xtrace
 # set -eox pipefail #safety for script
 
   echo "===================================================================================="
